84519a235fa95e4ebf4500321b6d05e026be09df
Jesper Grooss <jgr@dhigroup.com>
2019-12-20 16:27:27 +0100

    Updating build scripts for netstandard2.0

--- Examples/CSharp/DfsDiffBuild.bat
+++ Examples/CSharp/DfsDiffBuild.bat
@@ -1,4 +1,4 @@
 set csc=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe
 set sdkBin=..\..\bin
-%csc% -r:"%sdkBin%\DHI.Mike.Install.dll" -r:"%sdkBin%\DHI.Generic.MikeZero.DFS.dll" -r:"%sdkBin%\DHI.Generic.MikeZero.EUM.dll" -out:%sdkBin%\DfsDiff.exe DfsDiff.cs 
+%csc% -r:"%sdkBin%\DHI.Mike.Install.dll" -r:"%sdkBin%\DHI.Generic.MikeZero.DFS.dll" -r:"%sdkBin%\DHI.Generic.MikeZero.EUM.dll" -r:netstandard.dll -out:%sdkBin%\DfsDiff.exe DfsDiff.cs 
 REM pause
--- Examples/CSharp/MeshMergerBuild.bat
+++ Examples/CSharp/MeshMergerBuild.bat
@@ -1,4 +1,4 @@
 set csc=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe
 set sdkBin=..\..\bin
-%csc% -r:"%sdkBin%\DHI.Mike.Install.dll" -r:"%sdkBin%\DHI.Generic.MikeZero.DFS.dll" -r:"%sdkBin%\DHI.Generic.MikeZero.EUM.dll" -out:%sdkBin%\MeshMerger.exe MeshMerger.cs
+%csc% -r:"%sdkBin%\DHI.Mike.Install.dll" -r:"%sdkBin%\DHI.Generic.MikeZero.DFS.dll" -r:"%sdkBin%\DHI.Generic.MikeZero.EUM.dll" -r:netstandard.dll -out:%sdkBin%\MeshMerger.exe MeshMerger.cs
 REM pause
